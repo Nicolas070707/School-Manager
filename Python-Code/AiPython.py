c41ecc465d3d9c0c832c2dc4a852adcafa6113b6
Simi-Mini <simon.rapp@student.htldornbirn.at>
2023-12-18 12:21:15 +0100

    Update AiPython.py
    automatisch anmelden

--- Python-Code/AiPython.py
+++ Python-Code/AiPython.py
@@ -22,6 +22,7 @@ except ImportError:
 # Setze den Pfad zum ausführbaren ChromeDriver
 chromedriver_path = 'C:\\3bwi\\SWP\\Schoolmanager\\chromedriver_win32\\chromedriver.exe'
 
+browser = chromedriver_path
 # Setze die Chrome-Optionen
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument(f'--webdriver-path={chromedriver_path}')
@@ -58,7 +59,6 @@ try:
 except TimeoutException:
     print("Zeitüberschreitung beim Warten auf das Erscheinen des Links 'WebUntis'.")
 
-
 # Warte darauf, dass die Seite nach dem Klicken auf den Link "WebUntis" geladen wird
 WebDriverWait(driver, 20).until(EC.title_contains('WebUntis'))
 
@@ -79,7 +79,6 @@ try:
     print("Seite vollständig geladen.")
 except TimeoutException:
     print("Zeitüberschreitung beim Warten auf das vollständige Laden der Seite.")
-
 
 # Klicke auf den Text "HTL Dornbirn" (ersetze dies durch den tatsächlichen Locator)
 try:
@@ -129,42 +128,45 @@ time.sleep(5)
 
 driver.switch_to.active_element.send_keys('nicolas.theiner@student.htldornbirn.at')
 
-time.sleep(40)
-
-# Maximiere das Browser-Fenster auf Vollbild
-driver.fullscreen_window()
+driver.switch_to.active_element.send_keys(Keys.ENTER)
 
 time.sleep(5)
 
-# Funktion, um die Farbe in allen Elementen zu überprüfen
-def rgba_to_rgb(color):
-    """Konvertiert RGBA zu RGB, falls nötig."""
-    match = re.match(r'rgba\((\d+), (\d+), (\d+), [\d.]+\)', color)
-    if match:
-        return f'rgb({match.group(1)}, {match.group(2)}, {match.group(3)})'
-    return color
-# Stellen Sie sicher, dass die Farbe im richtigen Format ist
-color_to_check = 'rgb(196, 198, 198)' # #dedfdf in RGB
-def check_color_in_elements(driver, color):
-    elements = driver.find_elements(By.XPATH, "//*")
-    for element in elements:
-        background_color = element.value_of_css_property('background-color')
-        color_property = element.value_of_css_property('color')
+driver.switch_to.active_element.send_keys('ProvisorischesKW!123')
 
-        # Konvertiere die Farben von RGBA zu RGB, falls nötig
-        background_color = rgba_to_rgb(background_color)
-        color_property = rgba_to_rgb(color_property)
+driver.switch_to.active_element.send_keys(Keys.ENTER)
 
-        if color in [background_color, color_property]:
-            print(f"Farbe gefunden in Element mit Tag: {element.tag_name}, Klasse: {element.get_attribute('class')}, ID: {element.get_attribute('id')}")
-            return True
+time.sleep(5)
 
-    return False
+driver.switch_to.active_element.send_keys(Keys.ENTER)
 
-# Überprüfe, ob die Farbe vorhanden ist
-if check_color_in_elements(driver, color_to_check):
-    print("Farbcode gefunden.")
-else:
-    print("Farbcode nicht gefunden.")
+first_focusable_element = driver.find_element(By.TAG_NAME, 'body')
+first_focusable_element.click()
+
+for _ in range(100):  # Begrenzen Sie die Anzahl der Versuche
+    # Drücken Sie die Tabulator-Taste
+    first_focusable_element.send_keys(Keys.TAB)
+    time.sleep(0.5)  # Kurze Verzögerung
+
+    # Überprüfen Sie das aktuell fokussierte Element
+    focused_element_text = driver.execute_script(
+        "return document.activeElement.textContent || document.activeElement.innerText;"
+    )
+
+    # Ändern Sie diese Zeile, um auf das Wort "Stundenplan" zu reagieren
+    if "Mein Stundenplan" in focused_element_text:
+        print("Das Wort 'Mein Stundenplan' gefunden und fokussiert.")
+        time.sleep(1)  # Kurze Wartezeit
+        break
+
+
+time.sleep(5)
+
+driver.execute_script("document.body.style.zoom='50%'")
+
+time.sleep(5)
+# Maximiere das Browser-Fenster auf Vollbild
+driver.fullscreen_window()
 
 time.sleep(180)
+
